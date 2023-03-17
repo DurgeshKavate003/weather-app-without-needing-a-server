d80fcc8f26715a0fe437e38146d1d29df46e714e
DurgeshKavate003 <durgesh.kavate@outlook.com>
2023-03-17 16:51:00 +0530

    Commit #2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
-import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    base: '/weather-app-without-needing-a-server/',
-    plugins: [vue()]
+    base: '/weather-app-without-needing-a-server/'
 })
